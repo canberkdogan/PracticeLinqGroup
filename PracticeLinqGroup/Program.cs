1a804a96af5b377ac6a543051db0c7af8186d1c3
canbe <canbe@CANBERK>
2024-09-06 22:57:55 +0300

    Add project files.

---
+++ PracticeLinqGroup/Program.cs
@@ -0,0 +1,38 @@
+﻿using PracticeLinqGroup;
+
+var studentList = new List<Student>();
+studentList.Add(new Student { StudentId = 1, StudentName = "Ali", ClassId = 1 });
+studentList.Add(new Student { StudentId = 2, StudentName = "Ayşe", ClassId = 2 });
+studentList.Add(new Student { StudentId = 3, StudentName = "Mehmet", ClassId = 1 });
+studentList.Add(new Student { StudentId = 4, StudentName = "Fatma", ClassId = 3 });
+studentList.Add(new Student { StudentId = 5, StudentName = "Ahmet", ClassId = 2 });
+
+
+var classList = new List<Class>();
+
+classList.Add(new Class { ClassId = 1, ClassName = "Matematik" });
+classList.Add(new Class { ClassId = 2, ClassName = "Türkçe" });
+classList.Add(new Class { ClassId = 3, ClassName = "Kimya" });
+
+
+var studentAndClass = classList.GroupJoin(
+    studentList,
+    classes => classes.ClassId,
+    student => student.ClassId,
+    (classes, studentGroup) => new
+    {
+        ClassName = classes.ClassName,
+        Student = studentGroup
+    } 
+ ); 
+
+foreach ( var classes in studentAndClass)
+{
+    Console.WriteLine($"↓ {classes.ClassName} Dersi Alan Öğrenciler ↓");
+    foreach ( var student in classes.Student)
+    {
+        Console.WriteLine($"{student.StudentName}");
+    }
+}
+
+
